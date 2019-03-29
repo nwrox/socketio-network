16b385a7bc3ca3a84b4551a19d864c4451f4e534
Hérikz Nawarro <herikz.nawarro@gmail.com>
2019-03-29 15:41:27 -0300

    Create functions to send info to the socket server

---
+++ agent/index.js
@@ -0,0 +1,21 @@
+import io from 'socket.io-client'
+import { networkInterfaces, userInfo } from 'os'
+
+const socket = io(process.env.SOCKET_ADDR)
+
+const getIpAddresses = () => getPlainNetworkInterfaces(Object.values(networkInterfaces()))
+  .filter(({ family, internal }) => family === 'IPv4' && !internal)
+  .map(({ address }) => address)
+
+const getAgentData = () => ({
+  ip_addresses: getIpAddresses(),
+  username: getUsername(),
+})
+
+const getUsername = () => userInfo().username
+
+const getPlainNetworkInterfaces = nics => nics.reduce((acc, curr) => [...acc, ...curr], [])
+
+socket.on('get_agent_data', () => {
+  socket.emit('agent_data', getAgentData())
+})
