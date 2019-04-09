07fafb69e8151228b05a1bd0662735e6abafa21f
Hérikz Nawarro <herikz.nawarro@gmail.com>
2019-04-09 14:10:37 -0300

    [agent] Add emitter to logout agents from that ip address

--- agent/index.js
+++ agent/index.js
@@ -2,6 +2,13 @@ import { initSocket, registerEvents, sendData } from './socket'
 import { getAgentData, loadEnv } from './utils'
 
 loadEnv().then(({ SOCKET_ADDR }) => initSocket(SOCKET_ADDR))
-  .then(socket => sendData(socket, 'agent_data', getAgentData()))
+  .then(socket => {
+    const args = process.argv
+      .filter(arg => arg === '--logout')
+
+    return args.length
+      ? sendData(socket, 'agent_logout', getAgentData().ip_addresses)
+      : sendData(socket, 'agent_data', getAgentData())
+  })
   .then(socket => registerEvents(socket))
   .catch(console.error)
